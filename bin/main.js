9288a40a32e2decd23b1b267c9a327eda6d64610
urielch <uriel.chemouni@gmail.com>
2024-04-23 07:57:39 +0200

    add main

--- bin/main.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-const OpenCVBuilder = require("../dist/cjs/OpenCVBuilder.js");
-const builder = new OpenCVBuilder.OpenCVBuilder(process.argv);
-void builder.install();
--- bin/main.ts
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-import * as OpenCVBuilder from "../src/OpenCVBuilder";
-//= require("../dist/cjs/OpenCVBuilder.js");
-const builder = new OpenCVBuilder.OpenCVBuilder(process.argv);
-void builder.install();
